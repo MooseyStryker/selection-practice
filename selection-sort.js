f2f5d3a88799855b5a1c1b7154abb8a02cc17afd
Stryker Huber <stryker.important@gmail.com>
2023-10-09 15:24:17 -0700

    first commit

--- selection-sort.js
+++ selection-sort.js
@@ -1,24 +1,35 @@
 
 
 function selectionSort(arr) {
-
   // Copy the original array
+  const copy = arr.slice()
 
   // Create an array to store the sorted values
+  const sortedArr = []
 
   // While the array is not empty...
-
+  while (copy.length)
     // Do not move this console.log
     console.log(sorted.join(","));
 
     // Find the index of the minimum value in the unsorted half
+    let currentMinIndex = 0
+    for (let i = 0; i < copy.length; i++) {
+      if (copy[i] < copy[currentMinIndex]) {
+        currentMinIndex = i
+      }
 
-    // Save and remove the value at the min index
+      // Save and remove the value at the min index
+      const current
+    }
+
+
 
     // Add the min value to the end of the sorted array
 
 }
-
+arr = [2,4,6,8,1,3,5,7,9];
+selectionSort(arr) //[2,4,6,8,1,3,5,7,9]
 
 
 function selectionSortInPlace(arr) {
